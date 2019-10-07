a6e5f75e412d81e38e60875be42b3ad485969ded
Aleksei_Semenov <Aleksei_Semenov@epam.com>
2019-10-07 15:07:34 +0300

    First task (one part)

---
+++ src/main/java/entity/Task.java
@@ -0,0 +1,8 @@
+package entity;
+
+public class Task {
+    private Integer task_id;
+    private String description;
+    private Integer user_id;
+    private Boolean complete;
+}
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class User {
+    private Integer user_id;
+    private String user_name;
+    private String user_email;
+    private String password;
+    private Role user_role;
+    private String subscribe;
+}
