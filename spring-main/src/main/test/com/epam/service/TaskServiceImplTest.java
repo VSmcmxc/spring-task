7bef398856d452cf8df2ef943cea2739cd608661
Vadim Salimov <vadim.mcmxc@gmail.com>
2019-11-11 11:48:48 +0300

    some new

--- spring-main/src/main/test/com/epam/service/TaskServiceImplTest.java
+++ spring-main/src/main/test/com/epam/service/TaskServiceImplTest.java
@@ -1,15 +1,38 @@
 package com.epam.service;
 
+import com.epam.configuration.ApplicationConfig;
+import com.epam.configuration.DBConfig;
+import com.epam.entity.Priority;
+import com.epam.entity.Task;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfig.class, DBConfig.class})
 public class TaskServiceImplTest {
 
+    private Task task;
 
+    @Autowired
+    private TaskService taskService;
+
+    @Before
+    public void setup() {
+        task = taskService.getById(1l);
+    }
 
     @Test
     public void setTaskPriority() {
+        taskService.setTaskPriority(Priority.HIGHT, task);
+        assertTrue(task.getPriority().equals(Priority.HIGHT));
     }
 
     @Test
