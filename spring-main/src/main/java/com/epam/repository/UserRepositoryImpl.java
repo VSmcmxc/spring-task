b8ed35b1692e4c3cd91498e9083d1d1bfed5c0a1
Vadim Salimov <vadim.mcmxc@gmail.com>
2019-11-13 11:01:05 +0300

    save

--- spring-main/src/main/java/com/epam/repository/UserRepositoryImpl.java
+++ spring-main/src/main/java/com/epam/repository/UserRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.epam.repository;
 import com.epam.entity.User;
 import com.epam.entity.UserRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
